14a77b04305854115926070456e3c60deff6cfcb
Gaute Meek Olsen <gaute.meek@gmail.com>
2023-07-20 20:07:43 +0200

    fix footer

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { useQRCode } from '@vueuse/integrations/useQRCode'
-// @ts-ignore
-import { CreatedByMe } from 'gaute-web-components'
 
 const text = ref('https://gaute.dev')
 const qrcode = useQRCode(text, {
@@ -13,24 +11,11 @@ const qrcode = useQRCode(text, {
 </script>
 
 <template>
-  <main>
-    <input v-model="text" type="text" maxlength="2300">
-    <img :src="qrcode" alt="QR Code"/>
-  </main>
-  <footer>
-    <created-by-me></created-by-me>
-  </footer>
+  <input v-model="text" type="text" maxlength="2300">
+  <img :src="qrcode" alt="QR Code" />
 </template>
 
 <style scoped>
-main {
-  display: grid;
-  grid-template-rows: auto 1fr;
-  gap: 1rem;
-  justify-items: center;
-  padding: 1rem;
-}
-
 input {
   font-size: 1.5rem;
 }
@@ -39,11 +24,5 @@ img {
   border-radius: 5px;
   max-width: 75vw;
   max-height: 70vh;
-}
-
-footer {
-  display: grid;
-  place-items: center;
-  padding: 1rem;
 }
 </style>
