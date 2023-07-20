669390a1c3845a78544d76c050570f17d81725a5
Gaute Meek Olsen <gaute.meek@gmail.com>
2023-07-20 19:50:17 +0200

    first version

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useQRCode } from '@vueuse/integrations/useQRCode'
+// @ts-ignore
+import { CreatedByMe } from 'gaute-web-components'
+
+const text = ref('text-to-encode')
+const qrcode = useQRCode(text, {
+  quality: 1,
+  margin: 2,
+  width: 600,
+})
+</script>
+
+<template>
+  <main>
+    <input v-model="text" type="text" maxlength="2300">
+    <img :src="qrcode" alt="QR Code"/>
+  </main>
+  <footer>
+    <created-by-me></created-by-me>
+  </footer>
+</template>
+
+<style scoped>
+main {
+  display: grid;
+  grid-template-rows: auto 1fr;
+  gap: 1rem;
+  justify-items: center;
+  padding: 1rem;
+}
+
+input {
+  font-size: 1.5rem;
+}
+
+img {
+  border-radius: 5px;
+  max-width: 75vw;
+  max-height: 70vh;
+}
+
+footer {
+  display: grid;
+  place-items: center;
+  padding: 1rem;
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          // treat all tags with a dash as custom elements
+          // @ts-ignore
+          isCustomElement: (tag) => tag.includes('-')
+        }
+      }
+    })
+  ],
+})
